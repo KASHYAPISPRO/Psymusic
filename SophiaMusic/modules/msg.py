aa60392a203c467373b21e86cf41429ef2c10fa0
PSYR3X <90316018+PSYR3X@users.noreply.github.com>
2021-10-22 20:37:48 +0530

    Update msg.py
    wot is project name bruh?

--- SophiaMusic/modules/msg.py
+++ SophiaMusic/modules/msg.py
@@ -1,12 +1,12 @@
 import os
 from SophiaMusic.config import SOURCE_CODE,ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP,UPDATES_CHANNEL
 class Messages():
-      START_MSG = "**Hello 👋 [{}](tg://user?id={})!**\n\n🤖 I am an advanced bot created for playing music in the voice chats of Telegram Groups & Channels.\n\n✅ Send me /help for more info."
+      START_MSG = "**Hello 👋 [{}](tg://user?id={})!**\n\n🤖 I am not a very advanced bot created for playing music in the voice chats of Tg Groups & Channels.\n\n✅ Type /help for more info. \n\nCurrently this bot is under psyrex production. \n\nyou cant add it any where hehehe"
       HELP_MSG = [
         ".",
 f"""
 **Hey 👋 Welcome back to {PROJECT_NAME}
-⚪️ {PROJECT_NAME} can play music in your group's voice chat as well as channel voice chats
+⚪️ {PROJECT_NAME} can play music in group's voice chat as well as channel voice chats
 ⚪️ Assistant name >> @{ASSISTANT_NAME}\n\nClick next for instructions**
 """,
 
@@ -22,17 +22,17 @@ f"""
 3) Now send commands in linked group
 **Commands**
 **=>> Song Playing 🎧**
-- /play: Play the requestd song
-- /play [yt url] : Play the given yt url
-- /play [reply yo audio]: Play replied audio
-- /splay: Play song via jio saavn
-- /ytplay: Directly play song via Youtube Music
+- /play: Plays the requestd song
+- /play [yt url] : Plays audio from given yt url
+- /play [reply yo audio]: Plays replied audio
+- /splay: Plays song via jio saavn
+- /ytplay: Directly plays song via Youtube Music
 **=>> Playback ⏯**
 - /player: Open Settings menu of player
 - /skip: Skips the current track
 - /pause: Pause track
 - /resume: Resumes the paused track
-- /end: Stops media playback
+- /end: Kills media playback
 - /current: Shows the current Playing track
 - /playlist: Shows playlist
 *Player cmd and all other cmds except /play, /current  and /playlist  are only for admins of the group.
@@ -41,18 +41,18 @@ f"""
 f"""
 **=>> Channel Music Play 🛠**
 ⚪️ For linked group admins only:
-- /cplay [song name] - play song you requested
-- /csplay [song name] - play song you requested via jio saavn
-- /cplaylist - Show now playing list
-- /cccurrent - Show now playing
-- /cplayer - open music player settings panel
-- /cpause - pause song play
-- /cresume - resume song play
-- /cskip - play next song
-- /cend - stop music play
+- /cplay [song name] - plays song you requested
+- /csplay [song name] - plays song you requested via jio saavn
+- /cplaylist - Shows now playing list
+- /cccurrent - Shows now playing song
+- /cplayer - opens music player settings panel
+- /cpause - pause track
+- /cresume - resume track
+- /cskip - plays next song
+- /cend - Kills music play
 - /userbotjoinchannel - invite assistant to your chat
 channel is also can be used instead of c ( /cplay = /channelplay )
-⚪️ If you donlt like to play in linked group:
+⚪️ If you don't like to play in linked group:
 1) Get your channel ID.
 2) Create a group with tittle: Channel Music: your_channel_id
 3) Add bot as Channel admin with full perms
@@ -63,8 +63,8 @@ channel is also can be used instead of c ( /cplay = /channelplay )
 f"""
 **=>> More tools 🧑‍🔧**
 - /musicplayer [on/off]: Enable/Disable Music player
-- /admincache: Updates admin info of your group. Try if bot isn't recognize admin
-- /userbotjoin: Invite @{ASSISTANT_NAME} Userbot to your chat
+- /admincache: Updates admin info of your group. </ Try if bot isn't recognize admin />
+- /userbotjoin: Invites @{ASSISTANT_NAME} Userbot to your chat
 - /auth [reply to user] - Authorize User
 - /deauth [reply to user] - DeAuthorize user
 Authorized users can execute admin commands in authorized group
